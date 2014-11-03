5aaa2e387394e079237b0e0524c085836ae46c8d
jbzdarkid <jbzdarkid@users.noreply.github.com>
2014-11-02 20:07:50 -0500

    Initial commit

--- ButtonModule/src/main/java/ButtonModule/buttonModule/ButtonModule.java
+++ ButtonModule/src/main/java/ButtonModule/buttonModule/ButtonModule.java
@@ -38,6 +38,7 @@ public class ButtonModule implements IJanewayModule {
 		buttonPanel.setLayout(new FlowLayout());
 		JButton button0 = new JButton("Button 0");
 		JButton button1 = new JButton("Button 1");
+		JButton buttonJoseph = new JButton("Joseph");
 		
 		// Setup the main panel
 		JPanel mainPanel = new JPanel();
@@ -51,6 +52,8 @@ public class ButtonModule implements IJanewayModule {
 		buttonPanel.add(button0);
 		button1.addActionListener(new TestListener("You pushed button 1", text));
 		buttonPanel.add(button1);
+		buttonJoseph.addActionListener(new TestListener("Blackman", text));
+		butonPanel.add(buttonJoseph);
 		
 		tabs = new ArrayList<JanewayTabModel>();
 		JanewayTabModel tab = new JanewayTabModel("//no comment", new ImageIcon(), buttonPanel, mainPanel);
