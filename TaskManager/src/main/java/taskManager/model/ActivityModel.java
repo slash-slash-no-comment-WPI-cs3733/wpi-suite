7078a399b46b0e3364d3ea73b3baa5041dd3ba96
samkhal <samkhal@gmail.com>
2014-11-06 14:23:30 -0500

    Implemented basic ActivityModel

--- TaskManager/src/main/java/taskManager/model/ActivityModel.java
+++ TaskManager/src/main/java/taskManager/model/ActivityModel.java
@@ -8,10 +8,76 @@
  *******************************************************************************/
 package taskManager.model;
 
+import java.util.Date;
+
+import edu.wpi.cs.wpisuitetng.modules.core.models.User;
+
 /**
- * @author Sam Khalandovsky
+ * Description
  *
+ * @author Sam Khalandovsky
+ * @version Nov 6, 2014
  */
 public class ActivityModel implements IModel {
+
+	// Static ID counter; incremented each time task is created. Rollover can be
+	// safely ignored.
+	private static int idCount = 0;
+
+	// Unique identifier
+	private final int id;
+
+	// Date of creation
+	private final Date dateCreated;
+
+	// Contents of activity
+	private final String description;
+
+	// User who took the action; null for system activities
+	private final User actor;
+
+	/**
+	 * Constructor for automatic activities
+	 *
+	 * @param description
+	 */
+	public ActivityModel(String description) {
+		this(description, null);
+	}
+
+	/**
+	 * Constructor for activities with user actor
+	 *
+	 * @param description
+	 * @param actor
+	 */
+	public ActivityModel(String description, User actor) {
+		this.description = description;
+		this.actor = actor;
+		id = idCount++;
+		dateCreated = new Date(); // set date to time ActivityModel was
+									// instantiated
+	}
+
+	/**
+	 * @return the dateCreated
+	 */
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	/**
+	 * @return the contents
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @return the actor
+	 */
+	public User getActor() {
+		return actor;
+	}
 
 }
