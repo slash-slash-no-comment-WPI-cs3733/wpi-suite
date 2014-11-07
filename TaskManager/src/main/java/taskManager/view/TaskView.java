89823aacd88d9243021ef774173860c84da2ff42
emartino <emartino@wpi.edu>
2014-11-07 03:30:14 -0500

    Adds interfaces for TaskView, WorkflowView, CommentView and StageView. Adds UI components for Task, Stage and Workflow with example stages and tasks

--- TaskManager/src/main/java/taskManager/view/TaskView.java
+++ TaskManager/src/main/java/taskManager/view/TaskView.java
@@ -1,33 +1,49 @@
 package taskManager.view;
 
-import java.awt.LayoutManager;
+/*
+ * @author Beth Martino
+ */
 
-import javax.swing.JPanel;
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
 
-public class TaskView extends JPanel {
+import java.awt.*;
+
+public class TaskView extends JPanel implements ITaskView {
 
 	/**
-	 * 
+	 * not sure what this is, ask Jon or Sam?
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	public TaskView() {
-		// TODO Auto-generated constructor stub
+	/**
+	 * @param the name of the new task
+	 * creates a list-like view for the following information
+	 * the name of the task, the due date and the estimated effort
+	 */
+	public TaskView(String name){
+		//organizes the data in a vertical list
+		this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+		Border raisedbevel = BorderFactory.createRaisedBevelBorder();
+		TitledBorder title = BorderFactory.createTitledBorder(
+                raisedbevel, name);
+		title.setTitlePosition(TitledBorder.LEFT);
+		this.setBorder(title);
+		this.setMinimumSize(new Dimension(300,100));
+		
+		
+		//adds the data to the view
+		this.add(new JLabel("Due Date: 00/00/00")); 
+		this.add(new JLabel("Est Effort: 3#"));
+	}
+	
+	
+	@Override
+	public String getName(){
+		return this.getName();
 	}
 
-	public TaskView(LayoutManager arg0) {
-		super(arg0);
-		// TODO Auto-generated constructor stub
-	}
-
-	public TaskView(boolean arg0) {
-		super(arg0);
-		// TODO Auto-generated constructor stub
-	}
-
-	public TaskView(LayoutManager arg0, boolean arg1) {
-		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
-	}
+	
 
 }
