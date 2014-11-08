4802ab596c1049c86de2d450c9acdcd16b4f8a99
emartino <emartino@wpi.edu>
2014-11-08 15:16:20 -0500

    Moves adding task views to scroll pane into stage view

--- TaskManager/src/main/java/taskManager/view/StageView.java
+++ TaskManager/src/main/java/taskManager/view/StageView.java
@@ -4,6 +4,9 @@ package taskManager.view;
  * @author Beth Martino
  */
 
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.*;
 
 public class StageView extends JPanel implements IStageView {
@@ -13,34 +16,52 @@ public class StageView extends JPanel implements IStageView {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	JPanel tasks = new JPanel();
+	JScrollPane stage = new JScrollPane(tasks);
+	
 
-	public StageView() {
+	public StageView(String name) {
+		
+		//stage view is a panel that contains the title and the scroll pane w/tasks
+		this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
 		
 		//organizes the tasks in a vertical list
-		this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+		tasks.setLayout(new BoxLayout(tasks,BoxLayout.Y_AXIS));
+		
+		
+		//creates the label for the name of the stage and adds it to the block
+		JPanel label = new JPanel();
+		label.setPreferredSize(new Dimension(175,25));
+		label.add(new JLabel(name));
+		this.add(label);
+		
+		//creates the scroll containing the stage view and adds it to the block
+		stage.setBorder(BorderFactory.createLineBorder(Color.black));
+		stage.setMinimumSize(new Dimension(175,350));
+		this.add(stage);
 		
 		//adds example tasks
 		this.addTaskView("Task1");
-		this.addTaskView("Task2");
-		this.addTaskView("Task3");
-		this.addTaskView("Task4");
-		this.addTaskView("Task5");
-		this.addTaskView("Task5");
-		this.addTaskView("Task5");
-		this.addTaskView("Task5");
-		this.addTaskView("Task5");
-		this.addTaskView("Task5");
-		this.addTaskView("Task5");
-		this.addTaskView("Task5");
-
+		this.addTaskView("Task1");
+		this.addTaskView("Task1");
+		this.addTaskView("Task1");
+		this.addTaskView("Task1");
+		updateTasks();
 	}
+	
+	private void updateTasks(){
+		this.remove(stage);
+		stage  = new JScrollPane(tasks);
+		this.add(stage);
+	}
+	
 	
 	/*
 	 * @param data for new task view
 	 * will be entered by the user
 	 */
 	public void addTaskView(String name){
-		this.add(new TaskView(name));
+		tasks.add(new TaskView(name));
 	}
 	
 	@Override
--- TaskManager/src/main/java/taskManager/view/WorkflowView.java
+++ TaskManager/src/main/java/taskManager/view/WorkflowView.java
@@ -4,6 +4,7 @@ package taskManager.view;
  * @author Beth Martino
  */
 import java.awt.*;
+import javax.swing.*;
 
 import javax.swing.*;
 
@@ -33,25 +34,7 @@ public class WorkflowView extends JPanel implements IWorkflowView{
 	 * sets the size and border
 	 */
 	public void addStageView(String name){
-		
-		//creates the container for both the label and the scroll
-		JPanel block = new JPanel();
-		block.setLayout(new BoxLayout(block,BoxLayout.Y_AXIS));
-		
-		//creates the label for the name of the stage and adds it to the block
-		JPanel label = new JPanel();
-		label.setPreferredSize(new Dimension(175,25));
-		label.add(new JLabel(name));
-		block.add(label);
-		
-		//creates the scroll containing the stage view and adds it to the block
-		JScrollPane stage = new JScrollPane(new StageView());
-		stage.setBorder(BorderFactory.createLineBorder(Color.black));
-		stage.setPreferredSize(new Dimension(175,350));
-		block.add(stage);
-		
-		//adds the combined label and scroll to the workflow view
-		this.add(block);
+		this.add(new StageView(name));
 	}
 	
 	@Override
