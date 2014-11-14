3573dbea90298c78d88698da43544c3d4fc671df
Jon Sorrells <jlsorrells@wpi.edu>
2014-11-13 22:07:03 -0500

    fix comment

--- TaskManager/src/main/java/taskManager/controller/WorkflowController.java
+++ TaskManager/src/main/java/taskManager/controller/WorkflowController.java
@@ -32,7 +32,8 @@ public class WorkflowController {
 	/**
 	 * Constructor for the WorkflowController, gets all the stages from the
 	 * WorkflowView, creates the corresponding StageView and StageControllers,
-	 * and adds the StageViews to the UI.
+	 * and adds the StageViews to the UI. Polls the server every 1 second until
+	 * it receives the workflow model.
 	 * 
 	 * @param view
 	 *            the corresponding WorkflowView object
