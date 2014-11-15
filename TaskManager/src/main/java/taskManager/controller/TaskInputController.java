d13f612a30225867ceefc89ccb4a49ae2fd78d34
emartino <emartino@wpi.edu>
2014-11-15 17:27:53 -0500

    fixes comments

--- TaskManager/src/main/java/taskManager/controller/TaskInputController.java
+++ TaskManager/src/main/java/taskManager/controller/TaskInputController.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2014 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package taskManager.controller;
 
 import java.awt.event.KeyEvent;
@@ -6,6 +14,13 @@ import java.awt.event.KeyListener;
 import taskManager.JanewayModule;
 import taskManager.view.EditTaskView;
 
+/**
+ * The controller for validating the inputs while editing or creating a task
+ * 
+ * @author Beth Martino
+ * @author Stefan Alexander
+ *
+ */
 public class TaskInputController implements KeyListener {
 
 	EditTaskView etv = JanewayModule.etv;
