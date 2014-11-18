8fca6c67f44ad7f9924f7e3563619f84ee28f94c
jbzdarkid <jbzdarkid@gmail.com>
2014-11-18 15:52:56 -0500

    Cleaned language on tests

--- TaskManager/src/test/java/taskManager/model/StageModelTest.java
+++ TaskManager/src/test/java/taskManager/model/StageModelTest.java
@@ -35,39 +35,37 @@ public class StageModelTest {
 
 	@Test
 	public void testTasklistSynchronizationDeletion() {
-		TaskModel task1 = new TaskModel("Task1", stage);
-		TaskModel task2 = new TaskModel("Task2", stage);
+		TaskModel task = new TaskModel("Task", stage);
 		wf = AbstractJsonableModel.fromJson(wf.toJson(), WorkflowModel.class);
 		wf.save();
 		stage = wf.findStageByName("Stage");
-		task2 = wf.findTaskByID("Task2");
-		stage.removeTask(task2);
+		task = wf.findTaskByID("Task");
+		stage.removeTask(task);
 		wf = AbstractJsonableModel.fromJson(wf.toJson(), WorkflowModel.class);
 		wf.save();
-		assertNull(wf.findStageByName("Stage").findTaskByID("Task2"));
+		assertNull(wf.findStageByName("Stage").findTaskByID("Task"));
 	}
 
 	@Test
 	public void testTasklistSynchronizationMove() {
 		StageModel stage2 = new StageModel(wf, "Stage2");
-		TaskModel task1 = new TaskModel("Task1", stage);
-		TaskModel task2 = new TaskModel("Task2", stage);
-		task2.setEstimatedEffort(5);
+		TaskModel task = new TaskModel("Task", stage);
+		task.setEstimatedEffort(5);
 		wf = AbstractJsonableModel.fromJson(wf.toJson(), WorkflowModel.class);
 		wf.save();
 		stage = wf.findStageByName("Stage");
 		stage2 = wf.findStageByName("Stage2");
-		task2 = wf.findTaskByID("Task2");
-		assertEquals(task2.getEstimatedEffort(), 5);
-		task2.setEstimatedEffort(10);
-		wf.moveTask(task2, stage, stage2);
+		task = wf.findTaskByID("Task");
+		assertEquals(task.getEstimatedEffort(), 5);
+		task.setEstimatedEffort(10);
+		wf.moveTask(task, stage, stage2);
 		wf = AbstractJsonableModel.fromJson(wf.toJson(), WorkflowModel.class);
 		wf.save();
 		stage = wf.findStageByName("Stage");
 		stage2 = wf.findStageByName("Stage2");
-		task2 = wf.findTaskByID("Task2");
-		assertNull(wf.findStageByName("Stage").findTaskByID("Task2"));
-		assertNotNull(wf.findStageByName("Stage2").findTaskByID("Task2"));
-		assertEquals(task2.getEstimatedEffort(), 10);
+		task = wf.findTaskByID("Task");
+		assertNull(wf.findStageByName("Stage").findTaskByID("Task"));
+		assertNotNull(wf.findStageByName("Stage2").findTaskByID("Task"));
+		assertEquals(task.getEstimatedEffort(), 10);
 	}
 }
