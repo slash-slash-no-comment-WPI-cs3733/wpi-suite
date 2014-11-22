b1cbc55ddf8487188d8b5c4d85a166f6abfc0c1d
Jon Sorrells <jlsorrells@wpi.edu>
2014-11-21 21:40:41 -0500

    print out list of sessions on db change

--- Core/WPISuite/src/main/java/edu/wpi/cs/wpisuitetng/WPICoreServlet.java
+++ Core/WPISuite/src/main/java/edu/wpi/cs/wpisuitetng/WPICoreServlet.java
@@ -12,10 +12,14 @@
 
 package edu.wpi.cs.wpisuitetng;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-import javax.servlet.http.*;
-import javax.servlet.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import edu.wpi.cs.wpisuitetng.exceptions.WPISuiteException;
 
@@ -27,59 +31,57 @@ import edu.wpi.cs.wpisuitetng.exceptions.WPISuiteException;
  * @author Mike Della Donna
  *
  */
-public class WPICoreServlet extends HttpServlet 
-{
+public class WPICoreServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -7156601241025735047L;
 	private ErrorResponseFormatter reponseFormatter;
-	
+
 	/**
 	 * Empty Constructor
 	 */
-	public WPICoreServlet()
-	{
+	public WPICoreServlet() {
 		this.reponseFormatter = new JsonErrorResponseFormatter();
 	}
-	
+
 	/**
-	 * Forwards get requests and restful parameters to the ManagerLayer singleton
+	 * Forwards get requests and restful parameters to the ManagerLayer
+	 * singleton
 	 */
-	public void doGet (HttpServletRequest req,
-                       HttpServletResponse res) throws ServletException, IOException
-	{
-        PrintWriter out = res.getWriter();
-        String delims = "[/]+";
-        String[] path = req.getPathInfo().split(delims);
-        
-        System.arraycopy(path, 1, path, 0, path.length-1);
-        path[path.length-1] = null;
-        
-        try {
-			out.println(ManagerLayer.getInstance().read(path,req.getCookies()));
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		PrintWriter out = res.getWriter();
+		String delims = "[/]+";
+		String[] path = req.getPathInfo().split(delims);
+
+		System.arraycopy(path, 1, path, 0, path.length - 1);
+		path[path.length - 1] = null;
+
+		try {
+			out.println(ManagerLayer.getInstance().read(path, req.getCookies()));
 		} catch (WPISuiteException e) {
 			res.setStatus(e.getStatus());
 		}
-       
-        out.close();
+
+		out.close();
 	}
-	
+
 	/**
-	 * Forwards put requests and restful parameters to the ManagerLayer singleton
+	 * Forwards put requests and restful parameters to the ManagerLayer
+	 * singleton
 	 */
-	public void doPut (HttpServletRequest req,
-            HttpServletResponse res) throws ServletException, IOException
-    {
+	public void doPut(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
 		BufferedReader in = req.getReader();
 		PrintWriter out = res.getWriter();
 		String delims = "[/]+";
-        String[] path = req.getPathInfo().split(delims);
-        
-        System.arraycopy(path, 1, path, 0, path.length-1);
-        path[path.length-1] = null;
-        
-        
-        try {
-        	out.println(ManagerLayer.getInstance().create(path,in.readLine(),req.getCookies()));
+		String[] path = req.getPathInfo().split(delims);
+
+		System.arraycopy(path, 1, path, 0, path.length - 1);
+		path[path.length - 1] = null;
+
+		try {
+			out.println(ManagerLayer.getInstance().create(path, in.readLine(),
+					req.getCookies()));
 		} catch (WPISuiteException e) {
 			res.setStatus(e.getStatus());
 			out.write(this.reponseFormatter.formatContent(e));
@@ -87,59 +89,66 @@ public class WPICoreServlet extends HttpServlet
 			out.close();
 			return;
 		}
-        res.setStatus(HttpServletResponse.SC_CREATED);
-        out.close();
-        
-    }
-	
+		res.setStatus(HttpServletResponse.SC_CREATED);
+		out.close();
+
+	}
+
 	/**
-	 * Forwards post requests and restful parameters to the ManagerLayer singleton
+	 * Forwards post requests and restful parameters to the ManagerLayer
+	 * singleton
 	 */
-	public void doPost (HttpServletRequest req,
-            HttpServletResponse res) throws ServletException, IOException
-    {
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
 		BufferedReader in = req.getReader();
 		PrintWriter out = res.getWriter();
 		String delims = "[/]+";
-        String[] path = req.getPathInfo().split(delims);
-        
-        System.arraycopy(path, 1, path, 0, path.length-1);
-        path[path.length-1] = null;
-        
-        try {
-			out.println(ManagerLayer.getInstance().update(path,in.readLine(),req.getCookies()));
+		String[] path = req.getPathInfo().split(delims);
+
+		System.arraycopy(path, 1, path, 0, path.length - 1);
+		path[path.length - 1] = null;
+
+		try {
+			out.println(ManagerLayer.getInstance().update(path, in.readLine(),
+					req.getCookies()));
+			ManagerLayer.getInstance().getSessions().getSessions()
+					.forEach((String s1, Session s2) -> {
+						System.out.println(s1 + s2.toString());
+					});
+			System.out.println();
 		} catch (WPISuiteException e) {
 			res.setStatus(e.getStatus());
 			out.write(this.reponseFormatter.formatContent(e));
 			out.flush();
 			out.close();
 		}
-        
-        out.close();
-    }
-	
+
+		out.close();
+	}
+
 	/**
-	 * Forwards delete requests and restful parameters to the ManagerLayer singleton
+	 * Forwards delete requests and restful parameters to the ManagerLayer
+	 * singleton
 	 */
-	public void doDelete (HttpServletRequest req,
-            HttpServletResponse res) throws ServletException, IOException
-    {
+	public void doDelete(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
 		PrintWriter out = res.getWriter();
 		String delims = "[/]+";
-        String[] path = req.getPathInfo().split(delims);
-        
-        System.arraycopy(path, 1, path, 0, path.length-1);
-        path[path.length-1] = null;
-        
-        try {
-			out.println(ManagerLayer.getInstance().delete(path,req.getCookies()));
+		String[] path = req.getPathInfo().split(delims);
+
+		System.arraycopy(path, 1, path, 0, path.length - 1);
+		path[path.length - 1] = null;
+
+		try {
+			out.println(ManagerLayer.getInstance().delete(path,
+					req.getCookies()));
 		} catch (WPISuiteException e) {
 			res.setStatus(e.getStatus());
 			out.write(this.reponseFormatter.formatContent(e));
 			out.flush();
 			out.close();
 		}
-        
-        out.close();
-    }
+
+		out.close();
+	}
 }
