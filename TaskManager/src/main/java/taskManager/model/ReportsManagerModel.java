c7df98328831d5f2e475dadbc34c71b76a8a9da0
Ezra <ezra.davis@gmail.com>
2014-11-22 20:22:11 -0500

    Adding comments

--- TaskManager/src/main/java/taskManager/model/ReportsManagerModel.java
+++ TaskManager/src/main/java/taskManager/model/ReportsManagerModel.java
@@ -20,6 +20,27 @@ public class ReportsManagerModel {
 		this.workflow = workflow;
 	}
 
+	/**
+	 * 
+	 * Returns the velocity (units of work completed) per day per user.
+	 *
+	 * Note: Not completely implemented, start and end don't work. Implement or
+	 * remove arguments.
+	 *
+	 * @param users
+	 *            The list of users we're finding data for
+	 * @param start
+	 *            The earliest date a task can be due by and still be included
+	 *            in the velocity calculations.
+	 * @param end
+	 *            The latest date a task can be due and still be included in the
+	 *            data.
+	 * @param averageCredit
+	 *            If false, splits the task's effort between users, otherwise
+	 *            gives all assigned users full credit for the task(s).
+	 * @return A map of Usernames to a Map of Dates to the amount of effort done
+	 *         by the user.
+	 */
 	public Map<String, Map<Date, Double>> getVelocity(Set<String> users,
 			Date start, Date end, boolean averageCredit) {
 		Map<String, Map<Date, Double>> data = new HashMap<String, Map<Date, Double>>();
@@ -47,6 +68,15 @@ public class ReportsManagerModel {
 		return data;
 	}
 
+	/**
+	 * 
+	 * Finds the list of all tasks associated with the given users.
+	 *
+	 * @param users
+	 *            The list of users that we're finding tasks for.
+	 * @return A map of Usernames to a list of tasks the user is associated
+	 *         with.
+	 */
 	public Map<String, List<TaskModel>> getStringTasks(Set<String> users) {
 		Map<String, List<TaskModel>> data = new HashMap<String, List<TaskModel>>();
 		for (String username : users) {
@@ -67,4 +97,5 @@ public class ReportsManagerModel {
 		}
 		return data;
 	}
+
 }
