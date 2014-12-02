90b0e7dad33e6c17f216beb71c661b686d5f014d
Samee-Swartz <Samee.swartz@gmail.com>
2014-12-01 20:40:49 -0500

    size fixes. Clark: (git)quickgiant (wpi)cwjacobson helped out

--- TaskManager/src/main/java/taskManager/view/TaskInfoPreviewView.java
+++ TaskManager/src/main/java/taskManager/view/TaskInfoPreviewView.java
@@ -72,13 +72,13 @@ public class TaskInfoPreviewView extends JPanel {
 		// The task's titleBar contains the title and the 'x' button
 		JPanel titleBar = new JPanel();
 		titleBar.setLayout(new MigLayout("", "0[]:push[]"));
-		titleBar.setSize(new Dimension(240, 30));
+		titleBar.setSize(new Dimension(245, 30));
 		JLabel title = new JLabel(this.taskM.getName());
 		title.setFont(title.getFont().deriveFont(15.0f));
-		title.setPreferredSize(new Dimension(230,
+		title.setPreferredSize(new Dimension(190,
 				title.getPreferredSize().height));
-		title.setSize(new Dimension(230, title.getPreferredSize().height));
-		title.setMaximumSize(new Dimension(235, title.getPreferredSize().height));
+		title.setSize(new Dimension(190, title.getPreferredSize().height));
+		title.setMaximumSize(new Dimension(190, title.getPreferredSize().height));
 		titleBar.add(title);
 		// Closable 'x' button
 		final JButton closeButton = new JButton("\u2716");
@@ -90,9 +90,8 @@ public class TaskInfoPreviewView extends JPanel {
 		info.add(titleBar);
 
 		// The task's description
-		// TODO: Make this have a '...' when it overflows
 		JTextArea description = new JTextArea();
-		description.setText(ellipsize(this.taskM.getDescription(), 150));
+		description.setText(ellipsize(this.taskM.getDescription(), 175));
 		description.setSize(new Dimension(210, 80));
 		description.setMaximumSize(new Dimension(210, 80));
 		description.setMinimumSize(new Dimension(210, 80));
@@ -211,25 +210,6 @@ public class TaskInfoPreviewView extends JPanel {
 		// Start by chopping off at the word before max
 		// the 3 is to account for '...'
 		int end = text.lastIndexOf(' ', max - 3);
-
-		// If text is one long word, this will add '...' in the word rather than
-		// split after the word
-		// if (end == -1) {
 		return text.substring(0, end) + "...";
-		// }
-
-		// Step forward as long as textWidth allows.
-		// int newEnd = end;
-		// do {
-		// end = newEnd;
-		// newEnd = text.indexOf(' ', end + 1);
-		//
-		// // No more spaces.
-		// if (newEnd == -1)
-		// newEnd = text.length();
-		//
-		// } while (textWidth(text.substring(0, newEnd) + "...") < max);
-
-		// return text.substring(0, end) + "...";
 	}
 }
