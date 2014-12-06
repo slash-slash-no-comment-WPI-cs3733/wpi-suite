e6a8c420d5f0545b02c4dcb5cf7e834a607f942b
samkhal <samkhal@gmail.com>
2014-12-06 02:22:27 -0500

    Redispatch drag over the WorkflowView down into the DropAreaPanel

--- TaskManager/src/main/java/taskManager/draganddrop/DropAreaPanel.java
+++ TaskManager/src/main/java/taskManager/draganddrop/DropAreaPanel.java
@@ -40,7 +40,6 @@ import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
 /**
@@ -412,7 +411,8 @@ class DropAreaListener implements DropTargetListener {
 			}
 			// Redispatch to ancestor drop target
 			if (ancestor != null) {
-				ancestor.getDropTarget().drop(convertCoords(ancestor, e));
+				ancestor.getDropTarget().drop(
+						DropTargetRedispatcher.convertCoords(e, ancestor));
 			}
 		}
 
@@ -464,7 +464,8 @@ class DropAreaListener implements DropTargetListener {
 			}
 			// Redispatch to ancestor drop target
 			if (ancestor != null) {
-				ancestor.getDropTarget().dragOver(convertCoords(ancestor, e));
+				ancestor.getDropTarget().dragOver(
+						DropTargetRedispatcher.convertCoords(e, ancestor));
 			}
 		}
 
@@ -479,45 +480,4 @@ class DropAreaListener implements DropTargetListener {
 
 	}
 
-	/**
-	 * Convert DropTargetDropEvent to the coordinate system of a different
-	 * component
-	 *
-	 * @param comp
-	 *            target component to convert coordinates to
-	 * @param e
-	 *            event to convert
-	 * @return converted event
-	 */
-	public DropTargetDropEvent convertCoords(Component comp,
-			DropTargetDropEvent e) {
-
-		Point newPoint = SwingUtilities.convertPoint(e.getDropTargetContext()
-				.getComponent(), e.getLocation(), comp);
-		DropTargetDropEvent newE = new DropTargetDropEvent(
-				e.getDropTargetContext(), newPoint, e.getDropAction(),
-				e.getSourceActions());
-		return newE;
-	}
-
-	/**
-	 * Convert DropTargetDragEvent to the coordinate system of a different
-	 * component
-	 *
-	 * @param comp
-	 *            target component to convert coordinates to
-	 * @param e
-	 *            event to convert
-	 * @return converted event
-	 */
-	public DropTargetDragEvent convertCoords(Component comp,
-			DropTargetDragEvent e) {
-
-		Point newPoint = SwingUtilities.convertPoint(e.getDropTargetContext()
-				.getComponent(), e.getLocation(), comp);
-		DropTargetDragEvent newE = new DropTargetDragEvent(
-				e.getDropTargetContext(), newPoint, e.getDropAction(),
-				e.getSourceActions());
-		return newE;
-	}
 }
---
+++ TaskManager/src/main/java/taskManager/draganddrop/DropTargetRedispatcher.java
@@ -0,0 +1,136 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2014 -- WPI Suite
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package taskManager.draganddrop;
+
+import java.awt.Component;
+import java.awt.Point;
+import java.awt.dnd.DropTargetDragEvent;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTargetEvent;
+import java.awt.dnd.DropTargetListener;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * This listener redispatches any incoming drag/drop events to a target
+ * DropTargetListener, and provides convenience methods for converting drag
+ * event coordinates
+ *
+ * @author Sam Khalandovsky
+ * @version Dec 6, 2014
+ */
+public class DropTargetRedispatcher implements DropTargetListener {
+
+	private final Component target;
+
+	/**
+	 * Constructor with target
+	 *
+	 * @param target
+	 *            Component to which events will be dispatched
+	 */
+	public DropTargetRedispatcher(Component target) {
+		if (target.getDropTarget() == null) {
+			throw new IllegalArgumentException(
+					"Component passed to DropTargetRedispatcher does not have a DropTarget");
+		}
+		this.target = target;
+	}
+
+	/*
+	 * @see
+	 * java.awt.dnd.DropTargetListener#dragEnter(java.awt.dnd.DropTargetDragEvent
+	 * )
+	 */
+	@Override
+	public void dragEnter(DropTargetDragEvent dtde) {
+		target.getDropTarget().dragEnter(convertCoords(dtde, target));
+	}
+
+	/*
+	 * @see
+	 * java.awt.dnd.DropTargetListener#dragOver(java.awt.dnd.DropTargetDragEvent
+	 * )
+	 */
+	@Override
+	public void dragOver(DropTargetDragEvent dtde) {
+		target.getDropTarget().dragOver(convertCoords(dtde, target));
+	}
+
+	/*
+	 * @see java.awt.dnd.DropTargetListener#dropActionChanged(java.awt.dnd.
+	 * DropTargetDragEvent)
+	 */
+	@Override
+	public void dropActionChanged(DropTargetDragEvent dtde) {
+		target.getDropTarget().dropActionChanged(convertCoords(dtde, target));
+	}
+
+	/*
+	 * @see
+	 * java.awt.dnd.DropTargetListener#dragExit(java.awt.dnd.DropTargetEvent)
+	 */
+	@Override
+	public void dragExit(DropTargetEvent dte) {
+		target.getDropTarget().dragExit(dte);
+	}
+
+	/*
+	 * @see
+	 * java.awt.dnd.DropTargetListener#drop(java.awt.dnd.DropTargetDropEvent)
+	 */
+	@Override
+	public void drop(DropTargetDropEvent dtde) {
+		target.getDropTarget().drop(convertCoords(dtde, target));
+	}
+
+	/**
+	 * Convert DropTargetDropEvent to the coordinate system of a different
+	 * component
+	 *
+	 * @param e
+	 *            event to convert
+	 * @param comp
+	 *            target component to convert coordinates to
+	 * @return converted event
+	 */
+	public static DropTargetDropEvent convertCoords(DropTargetDropEvent e,
+			Component comp) {
+
+		Point newPoint = SwingUtilities.convertPoint(e.getDropTargetContext()
+				.getComponent(), e.getLocation(), comp);
+		DropTargetDropEvent newE = new DropTargetDropEvent(
+				e.getDropTargetContext(), newPoint, e.getDropAction(),
+				e.getSourceActions());
+		return newE;
+	}
+
+	/**
+	 * Convert DropTargetDragEvent to the coordinate system of a different
+	 * component
+	 * 
+	 * @param e
+	 *            event to convert
+	 * @param comp
+	 *            target component to convert coordinates to
+	 *
+	 * @return converted event
+	 */
+	public static DropTargetDragEvent convertCoords(DropTargetDragEvent e,
+			Component comp) {
+
+		Point newPoint = SwingUtilities.convertPoint(e.getDropTargetContext()
+				.getComponent(), e.getLocation(), comp);
+		DropTargetDragEvent newE = new DropTargetDragEvent(
+				e.getDropTargetContext(), newPoint, e.getDropAction(),
+				e.getSourceActions());
+		return newE;
+	}
+
+}
