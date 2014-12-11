455c86fe3d601e174aa57524e6efab1b54fcfe6b
Clark Jacobsohn <clark.jacobsohn@gmail.com>
2014-12-10 21:02:30 -0500

    Fix graph scaling and turn bar gradients to solid colors.

--- TaskManager/src/main/java/taskManager/controller/ReportsManager.java
+++ TaskManager/src/main/java/taskManager/controller/ReportsManager.java
@@ -9,7 +9,6 @@
 
 package taskManager.controller;
 
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
@@ -39,6 +38,7 @@ import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.renderer.category.StandardBarPainter;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 import taskManager.JanewayModule;
@@ -334,7 +334,9 @@ public class ReportsManager implements ActionListener, ChangeListener,
 				);
 
 		final ChartPanel chartPanel = new ChartPanel(chart, false);
-		chartPanel.setPreferredSize(new Dimension(500, 270));
+		chartPanel.setMaximumDrawWidth(2880);
+		chartPanel.setMaximumDrawWidth(5120);
+		// chartPanel.setPreferredSize(new Dimension(500, 270));
 
 		final CategoryPlot plot = (CategoryPlot) chart.getPlot();
 
@@ -348,7 +350,11 @@ public class ReportsManager implements ActionListener, ChangeListener,
 		// disable bar outlines...
 		((BarRenderer) plot.getRenderer()).setDrawBarOutline(false);
 
-		return new ChartPanel(chart, false);
+		// disable gradients
+		((BarRenderer) plot.getRenderer())
+				.setBarPainter(new StandardBarPainter());
+
+		return chartPanel;
 	}
 
 	/**
